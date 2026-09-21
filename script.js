11d7d7fdcc586ba96ffb7028c9d0cf94a24222f2
club77_kmsdt <dhubert934@gmail.com>
2026-09-21 17:03:26 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+function warnCopy() {
+    alert("Bitte \n\n**Kopieren**\n\n Sie das Dokument, bevor Sie es ausfüllen.");
+}
+
+function openDoc1() {
+    warnCopy();
+    window.location.href = "https://docs.google.com/document/d/1vZn4uhzhglgnmRos1ZVOYMVM_2HUuotej5yNe_JADpc/edit?usp=sharing";
+}
+
+function openDoc2() {
+    warnCopy();
+    window.location.href = "https://docs.google.com/document/d/1ORNKrM9liqZiNcYIthWIAMraLlzau84lAJ5BP3BEGF8/edit?usp=sharing";
+}
+
+function openDoc3() {
+    warnCopy();
+    window.location.href = "https://docs.google.com/document/d/1fS57LHxl9iMnUWj13OK3kbo6AIsauZzQ/edit?usp=sharing&ouid=111122932360706081640&rtpof=true&sd=true";
+}
+
+function openMaintenance() {
+    alert("Aktuell noch in Bearbeitung");
+}
